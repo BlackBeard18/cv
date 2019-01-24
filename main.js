29889013e0eddce9c7572765e2b9e7c378386421
GEN-009 <gen-009@gen009-HP-ProBook-450-G3>
2019-01-24 11:59:27 +0100

    envoi du cv

---
+++ main.js
@@ -0,0 +1,8 @@
+//  appel de mes icone
+var iconLink = document.getElementById("img1");
+var iconGith = document.getElementById("img2");
+var iconFace = document.getElementById("img3");
+var iconTwee = document.getElementById("img4");
+
+// ci je clic sur un icone, alors je suis rediriger ver un lien lui corespondent..
+// ci je clic sur un des element de la bar de menu, alor la page lui corespondant souvre..
